80d7a251a0c1bde6bb70eb39f2631c8b1c38c6ac
meghan.lo <lo.meghan.e@gmail.com>
2021-01-16 14:51:04 -0500

    inital serach bar

---
+++ client/components/SearchBar.jsx
@@ -0,0 +1,57 @@
+import { SearchBar } from 'react-native-elements';
+// import React from 'react';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+// import { SearchBar } from 'react-native-elements';
+
+// export default class App extends React.Component {
+//   state = {
+//     search: '',
+//   };
+
+//   updateSearch = (search) => {
+//     this.setState({ search });
+//   };
+
+//   render() {
+//     const { search } = this.state;
+
+//     return (
+//       <SearchBar
+//         placeholder="Type Here..."
+//         onChangeText={this.updateSearch}
+//         value={search}
+//       />
+//     );
+//   }
+// }
+
+export default function CustomSearchBar(props) {
+    const [search, setSearch] = useState('');
+    
+    const updateSearch = (search) => {
+    setSearch({ search });
+    };
+
+    return (
+        // <View style={styles.container}>
+            <SearchBar
+            placeholder="Type Here..."
+            onChangeText={updateSearch}
+            value={search}
+            lightTheme={true}
+            // searchIcon={true}
+            />
+        // </View>
+    );
+}
+  
+const styles = StyleSheet.create({
+container: {
+    // flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+},
+});
---
+++ client/pages/Home.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import CustomSearchBar from '../components/SearchBar';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Search the web, offline tests.</Text>
+      {/* <StatusBar style="auto" /> */}
+      <CustomSearchBar/>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    // padding: '20',
+    // textAlign: 'center',
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 300
+  },
+});
