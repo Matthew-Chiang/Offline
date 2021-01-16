12abe97c49609192e5f16e8bfd3d51ad700d5f9f
Matthew <matthew.chiang.myc@gmail.com>
2021-01-16 12:34:33 -0500

    moved rapidAPI key to root level keys file

--- server/web_scraping/google_searh.py
+++ server/web_scraping/google_searh.py
@@ -1,13 +1,13 @@
 import requests
 import json
 
-with open('./scraping_keys.json') as file:
+with open('../keys.json') as file:
     keys = json.load(file)
 
 url = "https://google-search3.p.rapidapi.com/api/v1/search/q=weather toronto"
 
 headers = {
-    'x-rapidapi-key': keys["key"],
+    'x-rapidapi-key': keys["RAPID_API_KEY"],
     'x-rapidapi-host': "google-search3.p.rapidapi.com"
 }
 
