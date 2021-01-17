7e3c7e93045e705d426ed1d73c3793a4664d27ab
meghan.lo <lo.meghan.e@gmail.com>
2021-01-17 06:10:56 -0500

    trying to get backend connected

--- server/app.py
+++ server/app.py
@@ -1,5 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, make_response
 from pprint import pprint
+from flask.json import JSONEncoder
+import simplejson
+from send_sms import send_message
+from google_search import search_google
+# from web_scraping.generic_site import
 
 app = Flask(__name__)
 
@@ -16,10 +21,34 @@ def search():
 
 @app.route('/webhooks/inbound-sms', methods=['GET', 'POST'])
 def inbound_sms():
+    number = ''
+    text = ''
     if request.is_json:
-        pprint(request.get_json())
+        # print('JSON')
+        data_json = request.get_json()
+        pprint(data_json)
+        if data_json['type'] == 'text':
+            # print('TEXT')
+            number = data_json['msisdn']
+            text = data_json['text']
     else:
-        data = dict(request.form) or dict(request.args)
-        pprint(data)
+        # print('NOT JSON')
+        print(type(request))
+        pprint(request)
+        data = request.get_json()#(force=True)
+        # data = make_response(jsonify(request))
+        print(data)
+        print("TEST")
+        # response = MyResponseClass.force_type(request)
+        # print(JSONEncoder.default(request))
+        # data = dict(request.form) or dict(request.args)
+        # pprint(data)
+        if data['type'] == 'text':
+            number = data['msisdn']
+            text = data['text']
+    print('SEARCH TEXT ', text)
+    # search_results = search_google(text)
+    # print('SEARCH RESULTS',  search_results)
+    # send_message(number, search_results)
 
     return ('', 204)
--- server/web_scraping/generic_site.py
+++ server/web_scraping/generic_site.py
@@ -21,8 +21,23 @@ blacklist = [
     # there may be more elements you don't want, such as "style", etc.
 ]
 
+set = 0
+count = 0
+last_type = None
 for t in text:
-    if t.parent.name not in blacklist:
+    parent = t.parent.name
+    if parent not in blacklist and count < 1000:
+        if not (last_type == 'div' and last_type == parent):
+            if (parent == 'h1' or parent == 'h2' or parent =='h3'
+                    or parent == 'h4' or parent == 'h5' or parent == 'h6') and set == 0:
+                output += (parent + ' ')
+                set = 1
+            elif parent == 'span' and set == 0:
+                output += 'h6 '
+                set = 1
+            if t == '\n':
+                set = 0
         output += '{} '.format(t)
+        count += 1
 
 print(output)
