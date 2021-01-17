d3533efbcb025c1c4c158641dcec0061208bfe03
Matthew <matthew.chiang.myc@gmail.com>
2021-01-17 09:18:28 -0500

    navigate between pages

--- client/pages/Home.js
+++ client/pages/Home.js
@@ -1,47 +1,78 @@
+import SearchResultsCard from "../components/SearchResultsCard";
+import MediumCard from "../components/MediumCard";
+import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
 import CustomSearchBar from "../components/SearchBar";
-// import SendSMS from 'react-native-sms-x';
 
-// SendSMS.send(123, "+959254687254", "Hey.., this is me!\nGood to see you. Have a nice day.", (msg)=>{ alert(msg) });
+const { width } = Dimensions.get("window");
 
-export default function App() {
-    sendMessage = (number, message) => {
-        SendSMS.send(
-            123,
-            "14167860936",
-            "Hey.., this is me!\nGood to see you. Have a nice day.",
-            (msg) => {
-                alert(msg);
-            }
-        );
-    };
-
+export default function SearchResults() {
     return (
-        <View style={styles.container}>
-            <Text style={styles.headingText}>Search the web, offline.</Text>
-            <CustomSearchBar />
-        </View>
+        <ScrollView style={styles.scrollView}>
+            <View style={styles.main}>
+                <CustomSearchBar onEnter={() => {}} />
+                <View style={styles.textContainer}>
+                    <Text style={styles.heading}> Recipes </Text>
+                </View>
+
+                <ScrollView
+                    style={styles.horCarousel}
+                    showsHorizontalScrollIndicator={false}
+                    //pagingEnabled={true}
+                    horizontal={true}
+                    decelerationRate={0}
+                    snapToAlignment={"center"}
+                    contentInset={{
+                        top: 30,
+                        left: 30,
+                        bottom: 30,
+                        right: 30,
+                    }}
+                >
+                    <MediumCard />
+                    <MediumCard />
+                    <MediumCard />
+                    <MediumCard />
+                </ScrollView>
+
+                <View style={styles.textContainer}>
+                    <Text style={styles.heading2}> More Results </Text>
+                </View>
+                <View style={styles.searchResults}>
+                    <SearchResultsCard />
+                    <SearchResultsCard />
+                </View>
+            </View>
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
     main: {
         //flex: 1,
-        backgroundColor: "#000",
+        backgroundColor: "white",
         // alignItems: 'center',
         // justifyContent: 'center',
         // padding: '20',
         // textAlign: 'center',
         paddingLeft: 20,
         paddingRight: 20,
-        paddingTop: 300,
+        paddingTop: 50,
     },
-    headingText: {
-        fontSize: 18,
-        //   fontFamily: 'Inter',
-        color: "#4D4D4D",
-        paddingLeft: 10,
-        paddingBottom: 10,
+
+    heading: {
+        fontSize: 20,
+        color: "#30566B",
     },
+
+    heading2: {
+        fontSize: 16,
+    },
+
+    textContainer: {
+        padding: 10,
+    },
+
+    horCarousel: {},
 });
