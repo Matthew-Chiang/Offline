04915e75918180541521fb533cca27d054f73fc0
Matthew <matthew.chiang.myc@gmail.com>
2021-01-17 07:20:16 -0500

    got text from web hook

--- server/app.py
+++ server/app.py
@@ -21,34 +21,6 @@ def search():
 
 @app.route('/webhooks/inbound-sms', methods=['GET', 'POST'])
 def inbound_sms():
-    number = ''
-    text = ''
-    if request.is_json:
-        # print('JSON')
-        data_json = request.get_json()
-        pprint(data_json)
-        if data_json['type'] == 'text':
-            # print('TEXT')
-            number = data_json['msisdn']
-            text = data_json['text']
-    else:
-        # print('NOT JSON')
-        print(type(request))
-        pprint(request)
-        data = request.get_json()#(force=True)
-        # data = make_response(jsonify(request))
-        print(data)
-        print("TEST")
-        # response = MyResponseClass.force_type(request)
-        # print(JSONEncoder.default(request))
-        # data = dict(request.form) or dict(request.args)
-        # pprint(data)
-        if data['type'] == 'text':
-            number = data['msisdn']
-            text = data['text']
-    print('SEARCH TEXT ', text)
-    # search_results = search_google(text)
-    # print('SEARCH RESULTS',  search_results)
-    # send_message(number, search_results)
-
+    data = request.args.get('text')
+    print(data)
     return ('', 204)
